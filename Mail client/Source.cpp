876f7d71a4bae2bc5f488a68c8ce3a61bd64607c
artem314 <40601291+artem314@users.noreply.github.com>
2019-05-26 11:54:33 +0300

    Recipients

--- Mail client/Source.cpp
+++ Mail client/Source.cpp
@@ -32,10 +32,17 @@
 #define ID_DATA 2004
 #define ID_SEND 2005
 #define ID_FILE 2006
+#define ID_RECIPIENTS 2007
+#define ID_ADDRCPT 2008
+
 #define STD_LNG 512
 
 WSADATA ws;
 SSL_CTX *ctx;
+
+CRITICAL_SECTION c_section;
+
+TCHAR szBuffer[100] = TEXT("");
 
 char text[256];
 char Data[512];
@@ -103,21 +110,32 @@ HWND hFindDlg = NULL;
 HACCEL hAccel = NULL;
 
 OVERLAPPED _oRead = { 0 }, _oWrite = { 0 };
-char *lpszBufferText;
+//char *lpszBufferText;
 
 struct FILEstruct {
-	char fileName[256];
+	char fileName[260];
 	char *fileContent;
 	LARGE_INTEGER size;
 };
 
 std::vector<FILEstruct> files;
+std::vector<std::string> recipients;
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 void OnIdle(HWND hwnd);
 BOOL PreTranslateMessage(LPMSG lpMsg);
 BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct);
 void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify);
+
+INT_PTR  CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+BOOL Dialog_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam);
+
+void Dialog_OnClose(HWND hwnd);
+
+void Dialog_OnCommand(HWND hWnd, int id, HWND hwndCtl, UINT codeNotify);
+
+void OnAddItem(HWND hwnd, WPARAM wParam);
 
 int create_socket(int port, const char *hostname) {
 	int sockfd;
@@ -157,7 +175,7 @@ int send_SSLsocket(SSL *ssl,const char *buf)
 	int sended = 0;
 	while (all > 0)
 	{
-		sended = SSL_write(ssl, buf, strlen(buf));
+		sended = SSL_write(ssl, buf, all);
 		if (sended < 0)
 			return SOCKET_ERROR;
 		buf += sended;
@@ -168,13 +186,13 @@ int send_SSLsocket(SSL *ssl,const char *buf)
 
 bool checkstr(const char *buf,int len)
 {
-	
-	if (buf[len - 1] == '\n' && buf[len - 2] == '\r')
+	if (len > 2 && buf[len - 1] == '\n' && buf[len - 2] == '\r')
 	{
 		for (size_t i = len - 3; i > 0; i--)
 		{
 			if (buf[i] == '\n')
 			{
+				//isalpha()
 				char bufNumb[4];
 				memcpy(bufNumb, &buf[i+1], 3);
 				bufNumb[3] = '\0';
@@ -360,10 +378,10 @@ int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE, LPTSTR lpszCmdLine, int nCm
 
 	for (;;)
 	{
-		while (!PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE))
-		{
-			OnIdle(hwnd);
-		} // while
+		//while (!PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE))
+		//{
+		//	OnIdle(hwnd);
+		//} // while
 		bRet = GetMessage(&msg, NULL, 0, 0);
 
 		if (bRet == -1)
@@ -401,29 +419,29 @@ BOOL TryFinishIo(LPOVERLAPPED lpOverlapped)
 	return FALSE;
 } // TryFinishIo
 
-void OnIdle(HWND hwnd)
-{
-	if (NULL != lpszBufferText)
-	{
-		if (TryFinishIo(&_oRead) != FALSE) // асинхронное чтение данных из файла завершено
-		{
-			if (ERROR_SUCCESS == _oRead.Internal) // чтение завершено успешно
-			{
-				FILEstruct filesnd = { 0 };
-				filesnd.fileContent = new char[_oRead.InternalHigh];
-				filesnd.size.QuadPart = _oRead.InternalHigh;
-				memcpy(filesnd.fileContent, lpszBufferText, _oRead.InternalHigh);
-				files.push_back(filesnd);
-				
-				MessageBox(hwnd, "ЗАГРУЖЕНО", "", MB_ICONEXCLAMATION | MB_OK);
-			} // if
-
-			// освобождаем выделенную память
-			delete[] lpszBufferText, lpszBufferText = NULL;
-		} // if
-		
-	} // if
-} // OnIdle
+//void OnIdle(HWND hwnd)
+//{
+//	if (NULL != lpszBufferText)
+//	{
+//		if (TryFinishIo(&_oRead) != FALSE) // асинхронное чтение данных из файла завершено
+//		{
+//			if (ERROR_SUCCESS == _oRead.Internal) // чтение завершено успешно
+//			{
+//				FILEstruct filesnd = { 0 };
+//				filesnd.fileContent = new char[_oRead.InternalHigh];
+//				filesnd.size.QuadPart = _oRead.InternalHigh;
+//				memcpy(filesnd.fileContent, lpszBufferText, _oRead.InternalHigh);
+//				files.push_back(filesnd);
+//				
+//				MessageBox(hwnd, "ЗАГРУЖЕНО", "", MB_ICONEXCLAMATION | MB_OK);
+//			} // if
+//
+//			// освобождаем выделенную память
+//			delete[] lpszBufferText, lpszBufferText = NULL;
+//		} // if
+//		
+//	} // if
+//} // OnIdle
 
 BOOL PreTranslateMessage(LPMSG lpMsg)
 {
@@ -451,9 +469,21 @@ BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct)
 		WS_CHILD | WS_VISIBLE  |
 		ES_LEFT  |   WS_BORDER  , 10, 10, width - 40, 40, hwnd, (HMENU)ID_FROM, lpCreateStruct->hInstance, NULL);
 
-	CreateWindowEx(0, TEXT("Edit"), TEXT("artem_pochta_314@mail.ru"),
-		WS_CHILD | WS_VISIBLE |  
-		ES_LEFT  | WS_BORDER, 10, 60, width - 40, 40, hwnd, (HMENU)ID_TO, lpCreateStruct->hInstance, NULL);
+	//CreateWindowEx(0, TEXT("Edit"), TEXT("artem_pochta_314@mail.ru"),
+	//	WS_CHILD | WS_VISIBLE |  
+	//	ES_LEFT  | WS_BORDER, 10, 60, width - 40, 40, hwnd, (HMENU)ID_TO, lpCreateStruct->hInstance, NULL);
+
+
+	HWND cbmbx = CreateWindowEx(0, TEXT("COMBOBOX"), TEXT("Получатели"),
+		WS_CHILD | WS_VISIBLE  | WS_VSCROLL | CBS_DROPDOWNLIST , 10, 60, width /2 , 200,
+		hwnd, (HMENU)ID_RECIPIENTS, lpCreateStruct->hInstance, NULL);
+
+	ComboBox_AddString(cbmbx, "artem_pochta_314@mail.ru");
+	ComboBox_SetCurSel(cbmbx, 0);
+
+	CreateWindowEx(0, TEXT("Button"), TEXT("Добавить"),
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, width / 2 + 40, 60, width /3	, 40,
+		hwnd, (HMENU)ID_ADDRCPT, lpCreateStruct->hInstance, NULL);
 
 	CreateWindowEx(0, TEXT("Edit"), TEXT("Subject по русски"),
 		WS_CHILD | WS_VISIBLE |  
@@ -471,6 +501,8 @@ BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct)
 	CreateWindowEx(0, TEXT("Button"), TEXT("ФАЙЛ"),
 		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 10, 420, width / 2, 40,
 		hwnd, (HMENU)ID_FILE, lpCreateStruct->hInstance, NULL);
+
+	InitializeCriticalSection(&c_section);
 
 	return TRUE;
 }
@@ -499,6 +531,8 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			if (files[i].fileContent != NULL)
 				delete[] files[i].fileContent;
 		}
+
+		DeleteCriticalSection(&c_section);
 		PostQuitMessage(0);
 	}
 	break;
@@ -506,6 +540,14 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		DrawWindow();
 	}
+	break;
+
+	case WM_ADDITEM:
+	{
+		OnAddItem(hwnd, wParam);
+	}
+	return 0;
+
 	}
 
 	return DefWindowProc(hwnd, uMsg, wParam, lParam);
@@ -517,18 +559,16 @@ unsigned _stdcall SendMail(void *lpparameter)
 	SSL *ssl; 
 	ssl = SSL_new(ctx);
 
-	const char hostname[] = "smtp.gmail.com";
+	constexpr char hostname[] = "smtp.gmail.com";
 	int sock = create_socket(465, hostname);
-	if (sock != 0)
+	if (sock == FALSE)
 	{
-		printf("Successfully made the TCP connection to:%s\n", hostname);
+		return -1;
+		printf("Не удалось подключиться к серверу to:%s\n", hostname);
 	}
 	else
-		return -1;
 		
-	//DWORD timeout = 1000 ;
-	//setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(timeout));
-
+		
 	SSL_set_fd(ssl, sock);
 
 	if (SSL_connect(ssl) != 1)
@@ -536,12 +576,8 @@ unsigned _stdcall SendMail(void *lpparameter)
 		printf("Error: Could not build a SSL session to:%s\n", hostname);
 		return -1;
 	}
-	else
-	{
-		printf("Successfully enabled SSL/TLS session to: %s\n", hostname);
-	}
 		
-	send_SSLsocket(ssl,"EHLO  smtpclient\r\n");
+	send_SSLsocket(ssl,"EHLO smtpclient\r\n");
 	read_SSLsocket(ssl,true);
 	printf("%s\n", codeBuf);
 	printf("ehlo---------------------------------\n");
@@ -549,26 +585,28 @@ unsigned _stdcall SendMail(void *lpparameter)
 	send_SSLsocket(ssl, "AUTH LOGIN\r\n");
 	read_SSLsocket(ssl,false);
 	printf("%s\n", codeBuf);
-	printf("AUTH---------------------------------\n");
 
 	std::string login64 = "artem3331114@gmail.com";
+
+	//std::string login64 = "artem_korshunov_2012@mail.ru";
+
 	login64 = base64_encode(reinterpret_cast<const unsigned char*>(login64.c_str()), login64.length());
 	login64 = login64 + "\r\n";
 	send_SSLsocket(ssl, login64.c_str());
 	read_SSLsocket(ssl, false);
 	printf("%s\n", codeBuf);
-	printf("login---------------------------------\n");
+
 
 	std::string pass64 = "smjgzicjouudmlka";
+
 	pass64 = base64_encode(reinterpret_cast<const unsigned char*>(pass64.c_str()), pass64.length());
 	pass64 = pass64 + "\r\n";
 
 	send_SSLsocket(ssl, pass64.c_str());
 	read_SSLsocket(ssl, false);
 	printf("%s\n", codeBuf);
-	printf("pass---------------------------------\n");
 
-	std::string from = "artem3331114@gmail.com";
+	std::string from = "artem_korshunov_2012@mail.com";
 	from = base64_encode(reinterpret_cast<const unsigned char*>(from.c_str()), from.length());
 	from = "MAIL FROM: <" + from + ">\r\n";
 	send_SSLsocket(ssl, from.c_str());
@@ -586,11 +624,20 @@ unsigned _stdcall SendMail(void *lpparameter)
 	send_SSLsocket(ssl, "DATA\r\n");
 	read_SSLsocket(ssl, false);
 	printf("%s\n", codeBuf);
+	
+
+	//send_SSLsocket(ssl, "TO: artem_korshunov_2012@mail.com");
+
+	//std::string from1 = "RANDOM PAL";
+	//StringCchPrintfA(text, _countof(text), "FROM: %s <artem_@korshunov_2012@mail.ru>\r\n", from1.c_str());
+	//send_SSLsocket(ssl, text);
+
 
 	GetDlgItemTextA(hwnd, ID_SUBJECT, dlgtext, _countof(dlgtext));
 	StringCchPrintfA(text, _countof(text), "SUBJECT: %s\r\n", (LPSTR)dlgtext);
 	send_SSLsocket(ssl, text);
 	
+
 	HWND hwndctl = GetDlgItem(hwnd, ID_DATA);
 	int length = SendMessageA(hwndctl, WM_GETTEXTLENGTH, 0, 0);
 	LPSTR LpMessage = (LPSTR)malloc(length + 1);
@@ -635,6 +682,48 @@ unsigned _stdcall SendMail(void *lpparameter)
 	return 0;
 }
 
+unsigned _stdcall LoadFileAsync(void *lpparameter)
+{
+	char *filename = (char *)lpparameter;
+
+	FILEstruct file = { 0 };
+
+	HANDLE hExistingFile = CreateFile((char*)filename, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
+
+	if (INVALID_HANDLE_VALUE == hExistingFile) // не удалось открыть файл
+	{
+		return FALSE;
+	} // if
+
+	LARGE_INTEGER size;
+	BOOL bRet = GetFileSizeEx(hExistingFile, &size);
+
+	if ((FALSE != bRet) && (size.LowPart > 0))
+	{
+		file.fileContent = new char[size.QuadPart];
+
+		// начинаем асинхронное чтение данных из файла
+		bRet = ReadFile(hExistingFile, file.fileContent, size.QuadPart, NULL, NULL);
+
+		if (FALSE == bRet) // возникла ошибка
+		{
+			// освобождаем выделенную память
+			delete[] file.fileContent;
+		} // if
+		else
+		{
+			StringCchCopyA(file.fileName, _countof(file.fileName),(char*) filename);
+			file.size = size;
+			EnterCriticalSection(&c_section);
+			files.push_back(file);
+			LeaveCriticalSection(&c_section);
+		}
+		
+	} // if
+
+	return 0;
+}
+
 BOOL ReadAsync(HANDLE hFile, char *lpBuffer, DWORD dwOffset, DWORD dwSize, LPOVERLAPPED lpOverlapped)
 {
 	ZeroMemory(lpOverlapped, sizeof(OVERLAPPED));
@@ -654,39 +743,38 @@ BOOL ReadAsync(HANDLE hFile, char *lpBuffer, DWORD dwOffset, DWORD dwSize, LPOVE
 	return TRUE;
 } // ReadAsync
 
-BOOL OpenFileAsync(const char *szFileName)
-{
-	// открываем существующий файл для чтения и записи
-	HANDLE hExistingFile = CreateFile(szFileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_FLAG_OVERLAPPED, NULL);
-
-	if (INVALID_HANDLE_VALUE == hExistingFile) // не удалось открыть файл
-	{
-		return FALSE;
-	} // if
-
-	HANDLE hFile = hExistingFile;
-
-	// определяем размер файла 
-	LARGE_INTEGER size;
-	BOOL bRet = GetFileSizeEx(hFile, &size);
-
-	if ((FALSE != bRet) && (size.LowPart > 0))
-	{
-		// выделяем память для буфера, в который будет считываться данные из файла
-		lpszBufferText = new char[size.QuadPart];
-
-		// начинаем асинхронное чтение данных из файла
-		bRet = ReadAsync(hFile, lpszBufferText, 0, size.LowPart, &_oRead);
-
-		if (FALSE == bRet) // возникла ошибка
-		{
-			// освобождаем выделенную память
-			delete[] lpszBufferText, lpszBufferText = NULL;
-		} // if
-	} // if
-
-	return bRet;
-} // OpenFileAsync
+//BOOL OpenFileAsync(const char *szFileName)
+//{
+//	// открываем существующий файл для чтения и записи
+//	HANDLE hExistingFile = CreateFile(szFileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_FLAG_OVERLAPPED, NULL);
+//
+//	if (INVALID_HANDLE_VALUE == hExistingFile) // не удалось открыть файл
+//	{
+//		return FALSE;
+//	} // if
+//
+//	HANDLE hFile = hExistingFile;
+//	// определяем размер файла 
+//	LARGE_INTEGER size;
+//	BOOL bRet = GetFileSizeEx(hFile, &size);
+//
+//	if ((FALSE != bRet) && (size.LowPart > 0))
+//	{
+//		// выделяем память для буфера, в который будет считываться данные из файла
+//		lpszBufferText = new char[size.QuadPart];
+//
+//		// начинаем асинхронное чтение данных из файла
+//		bRet = ReadAsync(hFile, lpszBufferText, 0, size.LowPart, &_oRead);
+//
+//		if (FALSE == bRet) // возникла ошибка
+//		{
+//			// освобождаем выделенную память
+//			delete[] lpszBufferText, lpszBufferText = NULL;
+//		} // if
+//	} // if
+//
+//	return bRet;
+//} // OpenFileAsync
 
 void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
 {
@@ -702,8 +790,10 @@ void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
 
 		case ID_FILE :
 		{
+			files.capacity();
+
 			OPENFILENAME ofn;       
-			TCHAR szFile[260] = { 0 };       
+			char szFile[260] = { 0 };       
 
 			// Initialize OPENFILENAME
 			ZeroMemory(&ofn, sizeof(ofn));
@@ -720,13 +810,17 @@ void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
 
 			if (GetOpenFileName(&ofn) != FALSE)
 			{
-				if (OpenFileAsync(szFile) != FALSE) // открываем файл
-				{
-					
-				}
-			}
+				
+				HANDLE hp = (HANDLE)_beginthreadex(NULL, 0, LoadFileAsync,(char *)(szFile), 0, NULL);
+				//WaitForSingleObject(hp, INFINITE);
+				CloseHandle(hp);
 
-			
+				//if (OpenFileAsync(szFile) != FALSE) // открываем файл
+				//{
+				//	
+				//}
+			}
+						
 			/*WIN32_FIND_DATA fd;
 			HANDLE hFind = FindFirstFile("1.jpg", &fd);
 
@@ -746,10 +840,126 @@ void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
 		}
 		break;
 
+		case ID_ADDRCPT: //добавить  запись
+		{
+			HINSTANCE hInstance = GetWindowInstance(hwnd);
+
+			int nDlgResult = DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), hwnd, DialogProc);
+			if (IDOK == nDlgResult)
+			{
+				SendMessage(hwnd, WM_ADDITEM, 0, 0);
+			}
+		}
+		break;
+
+
 
 	}//switch
 }
 
+INT_PTR CALLBACK  DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		BOOL bRet = HANDLE_WM_INITDIALOG(hwndDlg, wParam, lParam, Dialog_OnInitDialog);
+		return SetDlgMsgResult(hwndDlg, uMsg, bRet);
+	}
+	case WM_CLOSE:
+		HANDLE_WM_CLOSE(hwndDlg, wParam, lParam, Dialog_OnClose);
+		return TRUE;
+	case WM_COMMAND:
+		HANDLE_WM_COMMAND(hwndDlg, wParam, lParam, Dialog_OnCommand);
+		return TRUE;
+	} // switch
+
+	return FALSE;
+}
+
+BOOL Dialog_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
+{
+	//  дескриптор окна редактируемого поля
+	HWND hwndEdit = GetDlgItem(hwnd, IDC_EDIT1);
+	//  максимальная длина текста в редактируемом поле
+	Edit_LimitText(hwndEdit, _countof(szBuffer) - 1);
+	return TRUE;
+}
+
+void Dialog_OnClose(HWND hwnd)
+{
+	if (hwnd == hDlg)
+	{
+		// уничтожаем немодальное диалоговое окно
+		DestroyWindow(hwnd);
+	}
+	else
+	{
+		// завершаем работу модального диалогового окна
+		EndDialog(hwnd, IDCLOSE);
+	}
+}
+
+void Dialog_OnCommand(HWND hWnd, int id, HWND hwndCtl, UINT codeNotify)
+{
+
+	switch (id)
+	{
+	case IDOK:
+	{
+		// получает содержимое редактируемого поля
+		int cch = GetDlgItemText(hWnd, IDC_EDIT1, szBuffer, _countof(szBuffer));
+
+		if (0 == cch) // в редактируемого поле нет текста
+		{
+			// получает дескриптор окна редактируемого поля
+			HWND hwndEdit = GetDlgItem(hWnd, IDC_EDIT1);
+
+			EDITBALLOONTIP ebt = { sizeof(EDITBALLOONTIP) };
+
+			ebt.pszTitle = L"Пусто";
+			ebt.pszText = L"Укажите название новой записи";
+			ebt.ttiIcon = TTI_WARNING;
+			Edit_ShowBalloonTip(hwndEdit, &ebt);
+		} // if
+		else if (hWnd == hDlg)
+		{
+			// очищает редактируемое поле
+			SetDlgItemText(hWnd, IDC_EDIT1, NULL);
+			// отправляет окну-владельцу сообщение о том, что нужно добавить запись
+			SendMessage(hwnd, WM_ADDITEM, 0, 0);
+		} // if
+		else
+		{
+			// завершает работу модального диалогового окна
+			EndDialog(hWnd, IDOK);
+		} // else
+	} // if
+	break;
+
+	case IDCANCEL: // нажата кнопка "Отмена"
+		if (hWnd == hDlg)
+		{
+			// уничтожает немодальное диалоговое окно
+			DestroyWindow(hWnd);
+		} // if
+		else
+		{
+			// завершает работу модального диалогового окна
+			EndDialog(hWnd, IDCANCEL);
+		} // else
+		break;
+	} // switch
+} // Dialog_OnCommand
+
+void OnAddItem(HWND hwnd, WPARAM wParam)
+{
+	HWND hwndCtl = GetDlgItem(hwnd, ID_RECIPIENTS);
+	// добавляем новый элемент в список
+	int iItem = ComboBox_AddItemData(hwndCtl, szBuffer);
+	// выделяем новый элемент
+	ComboBox_SetCurSel(hwndCtl, iItem);
+} // OnAddItem
 
 //MIME-Version:1.0
 //Content-Type:text/html
